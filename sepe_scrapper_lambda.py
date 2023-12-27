5b52113bbb5468200e01190f5bd05e8239852dba
Marcos Fernández Pérez <33698618+MarckFp@users.noreply.github.com>
2023-12-27 09:25:10 +0100

    fixes to work with curent api

--- sepe_scrapper_lambda.py
+++ sepe_scrapper_lambda.py
@@ -9,21 +9,21 @@ TELEGRAM_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
 #SEPE
 ID_CLIENTE = os.getenv('ID_CLIENTE')
-CODIGO_ENTIDAD = ""
+COD_ENTIDAD = os.getenv('COD_ENTIDAD')
+COD_IDIOMA = os.getenv('COD_IDIOMA')
 ID_GRUPO_SERVICIO = os.getenv('ID_GRUPO_SERVICIO')
 ZIPCODE = os.getenv('ZIPCODE')
 LAT_ORIGEN = os.getenv('LAT_ORIGEN')
 LNG_ORIGEN = os.getenv('LNG_ORIGEN')
 TRAMITE_RELACIONADO = os.getenv('TRAMITE_RELACIONADO')
 IDS_JERARQUIA_TRAMITES = os.getenv('IDS_JERARQUIA_TRAMITES')
-SEPE_URL = f'https://citaprevia-sede.sepe.gob.es/citapreviasepe/cita/cargaTiposAtencionMapa?idCliente={ID_CLIENTE}&codigoEntidad={CODIGO_ENTIDAD}&idGrupoServicio={ID_GRUPO_SERVICIO}&codigoPostal={ZIPCODE}&latOrigen={LAT_ORIGEN}&lngOrigen={LNG_ORIGEN}&tieneTramiteRelacionado={TRAMITE_RELACIONADO}&idsJerarquiaTramites={IDS_JERARQUIA_TRAMITES}'
-
+SEPE_URL = f'https://citaprevia-sede.sepe.gob.es/citapreviasepe/cita/cargaTiposAtencionMapa?idCliente={ID_CLIENTE}&codigoEntidad={CODIGO_ENTIDAD}&idGrupoServicio={ID_GRUPO_SERVICIO}&codigoPostal={ZIPCODE}&latOrigen={LAT_ORIGEN}&lngOrigen={LNG_ORIGEN}&tieneTramiteRelacionado={TRAMITE_RELACIONADO}&idsJerarquiaTramites={IDS_JERARQUIA_TRAMITES}&codIdioma={COD_IDIOMA}&codigoEntidad={COD_ENTIDAD}'
 def handler(_event, _context):
     POPUP_MESSAGE = "¡Cita disponible!"
     NO_APPOINTMENTS_MESSAGE = "No hay citas"
     ERROR_MESSAGE = "Ha ocurrido un error"
 
-    res = requests.get(SEPE_URL, timeout=20)
+    res = requests.post(SEPE_URL, timeout=20)
     if res.status_code == 200:
         local_time = time.localtime()
         current_time = time.strftime("%H:%M:%S", local_time)
